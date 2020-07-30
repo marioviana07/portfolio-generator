aedf85392768b9d20732158d9a50a1734714a2d5
marioviana07 <mario_viana_2005@yahoo.com>
2020-07-30 00:04:11 -0700

    Node input

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
 const profileDataArgs = process.argv.slice(2);
 const [name, github] = profileDataArgs;
+
+
+inquirer
+    .prompt([{
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }])
+    .then(answers => console.log(answers));
 
 // const generatePage = () => 'Name: Jane, Github: janehub';
 // const generatePage = (userName, githubName) => {
@@ -15,11 +25,11 @@ const [name, github] = profileDataArgs;
 
 
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//     if (err) throw new Error(err);
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
 // fs.writeFile('index.html', generatePage(name, github), err => {
 //     if (err) throw err;
